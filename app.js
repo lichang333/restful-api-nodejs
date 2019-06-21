77ef411f929a5777d12913ef5ee429b04d2740f8
lichang333 <lichang@oregonstate.edu>
2019-06-21 18:50:22 +0800

    finished today's fullstack challenge :)

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// const cors = require('cors'); //disable cors on codepen
+
+require('dotenv/config');
+
+// //middlewares
+// app.use('/posts', () => {
+//     console.log("middlewares running...");
+// });
+
+app.use(bodyParser.json());
+// app.use(cors());
+
+//import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 //routes
 app.get('/',(req,res) => {
     res.send('We are on home');
 });
 
+//connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => 
+    console.log('connected to DB!')
+);
 
 //listening to the server
 app.listen(9000);
---
+++ routes/posts.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+
+//get back all the posts
+router.get('/', async (req,res) => {
+    // res.send('We are on posts');
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message:err});
+    }
+});
+
+//submit a post
+router.post('/', (req,res) => {
+    // console.log(req.body);
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    post.save()
+        .then(data => {
+            res.json(data);
+    })
+    .catch(err => {
+        res.json({ message: err });
+    });
+});
+//specific post
+router.get('/:postId', async (req,res) => {
+    // console.log(req.params.postId);
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({message: err });
+    }
+    
+});
+
+//delete post
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({_id: req.params.postId });
+        res.json(removedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+    
+});
+
+//update a post
+router.patch('/:postId', async (req, res) => {
+    try{
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId }, 
+            {$set: {title: req.body.title} }
+        );
+        res.json(updatedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+
+})
+
+module.exports = router;
